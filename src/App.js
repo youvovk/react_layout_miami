7357af5a6e5012a749f390043fe5616261e8dcc7
Yurii Holiuk <bionicuss@gmail.com>
2019-09-05 11:05:02 +0300

    add scss support (#1)

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
-    <div>
-
+    <div className="page">
+      Hello React!
     </div>
   );
 }
